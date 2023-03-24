8f8a4c4443dca8bd046506fb69a066feb16c50f4
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 15:22:03 -0600

    Create Example_2.php

---
+++ PHPStrings/Example_2.php
@@ -0,0 +1,9 @@
+<!-- Count the number of words in a string - str_word_count() -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo str_word_count("Hello world!");
+        ?> 
+    </body>
+</html>
