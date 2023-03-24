85395effbe025953fc9e4869db82d3846b4fc8dc
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 16:17:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -72,5 +72,17 @@
             <li type="square"><a href="./PHPNumbers/Example_5.php">Example 5</a></li>
             <li type="square"><a href="./PHPNumbers/Example_6.php">Example 6</a></li>
         </details>
+
+        <h2>PHP Math</h2>
+        <details>
+            <summary>Examples</summary>
+            <li type="square"><a href="./PHPMath/Example_1.php">Example 1</a></li>
+            <li type="square"><a href="./PHPMath/Example_2.php">Example 2</a></li>
+            <li type="square"><a href="./PHPMath/Example_3.php">Example 3</a></li>
+            <li type="square"><a href="./PHPMath/Example_4.php">Example 4</a></li>
+            <li type="square"><a href="./PHPMath/Example_5.php">Example 5</a></li>
+            <li type="square"><a href="./PHPMath/Example_6.php">Example 6</a></li>
+            <li type="square"><a href="./PHPMath/Example_7.php">Example 7</a></li>
+        </details>
     </body>
 </html>
