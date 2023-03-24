e01be626eddedd9bb8b0d8d2225a7892d5130555
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 15:22:08 -0600

    Create Example_4.php

---
+++ PHPStrings/Example_4.php
@@ -0,0 +1,9 @@
+<!-- Search for a specific text within a string - strpos() -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo strpos("Hello world!", "world");
+        ?> 
+    </body>
+</html>
