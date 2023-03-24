4065989e94e800afbe5934c1ba7cb5971d72305b
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-23 19:07:14 -0600

    Create Example_3.php

---
+++ PHPSyntax/Example_3.php
@@ -0,0 +1,12 @@
+<!-- Variable names ARE case-sensitive -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $color = "red";
+            echo "My car is " . $color . "<br>";
+            echo "My house is " . $COLOR . "<br>";
+            echo "My boat is " . $coLOR . "<br>";
+        ?> 
+    </body>
+</html>
