be1d81b0f573eff6f91bf9492de2d38c98941817
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 16:26:00 -0600

    Create Example_1.php

---
+++ PHPConstants/Example_1.php
@@ -0,0 +1,11 @@
+<!-- Case-sensitive constant name -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            // case-sensitive constant name
+            define("GREETING", "Welcome to W3Schools.com!");
+            echo GREETING;
+        ?> 
+    </body>
+</html>
