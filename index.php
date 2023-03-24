da4fbcb3d1902dd1a7332575e13e725a7cefb5c1
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 14:44:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -40,5 +40,16 @@
             <li type="square"><a href="./PHPEchoAndPrint/Example_3.php">Example 3</a></li>
             <li type="square"><a href="./PHPEchoAndPrint/Example_4.php">Example 4</a></li>
         </details>
+
+        <h2>PHP Data Types</h2>
+        <details>
+            <summary>Examples</summary>
+            <li type="square"><a href="./PHPDataTypes/Example_1.php">Example 1</a></li>
+            <li type="square"><a href="./PHPDataTypes/Example_2.php">Example 2</a></li>
+            <li type="square"><a href="./PHPDataTypes/Example_3.php">Example 3</a></li>
+            <li type="square"><a href="./PHPDataTypes/Example_4.php">Example 4</a></li>
+            <li type="square"><a href="./PHPDataTypes/Example_5.php">Example 5</a></li>
+            <li type="square"><a href="./PHPDataTypes/Example_6.php">Example 6</a></li>
+        </details>
     </body>
 </html>
