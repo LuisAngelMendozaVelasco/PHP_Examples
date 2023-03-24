a21cbb6cf253e4298925d402e64273ce8522b110
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 16:01:14 -0600

    Create Example_3.php

---
+++ PHPNumbers/Example_3.php
@@ -0,0 +1,11 @@
+<!-- Check if a numeric value is finite or infinite -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            // Check if a numeric value is finite or infinite 
+            $x = 1.9e411;
+            var_dump($x);
+        ?>  
+    </body>
+</html>
