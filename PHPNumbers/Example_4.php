9d9c3cfa543366a40c23e7b7b65cc8a140c9bca5
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 16:01:16 -0600

    Create Example_4.php

---
+++ PHPNumbers/Example_4.php
@@ -0,0 +1,11 @@
+<!-- Invalid calculation will return a NaN value -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            // Invalid calculation will return a NaN value
+            $x = acos(8);
+            var_dump($x);
+        ?>  
+    </body>
+</html>
