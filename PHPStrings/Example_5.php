9df96ed6f718560ddb7c5199be80c2031e12f069
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-24 15:22:12 -0600

    Create Example_5.php

---
+++ PHPStrings/Example_5.php
@@ -0,0 +1,9 @@
+<!-- Replace text within a string - str_replace() -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo str_replace("world", "Dolly", "Hello world!");
+        ?> 
+    </body>
+</html>
