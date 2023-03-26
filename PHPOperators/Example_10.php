058e59a342bc898fafbe872364d1713a3d588d82
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:31 -0600

    Create Example_10.php

---
+++ PHPOperators/Example_10.php
@@ -0,0 +1,12 @@
+<!-- Assignment operator: x /= y -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 10;
+            $x /= 5;
+
+            echo $x;
+        ?>  
+    </body>
+</html>
