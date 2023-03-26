9f59ff3fa77a0700822097f6c5f9ae6666ce52b6
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:14:35 -0600

    Create Example_31.php

---
+++ PHPOperators/Example_31.php
@@ -0,0 +1,13 @@
+<!-- Logical operator: not -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 100;  
+
+            if ($x !== 90) {
+                echo "Hello world!";
+            }
+        ?>  
+    </body>
+</html>
