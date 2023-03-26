e0ee74cb86aa3d40adff7c30b6c351fe94860b29
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:14:08 -0600

    Create Example_21.php

---
+++ PHPOperators/Example_21.php
@@ -0,0 +1,24 @@
+<!-- Comparison operator: Spaceship (<=>) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 5;  
+            $y = 10;
+
+            echo ($x <=> $y); // returns -1 because $x is less than $y
+            echo "<br>";
+
+            $x = 10;  
+            $y = 10;
+
+            echo ($x <=> $y); // returns 0 because values are equal
+            echo "<br>";
+
+            $x = 15;  
+            $y = 10;
+
+            echo ($x <=> $y); // returns +1 because $x is greater than $y
+        ?>  
+    </body>
+</html>
