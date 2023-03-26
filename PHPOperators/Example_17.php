a4eb22f378c8fe85fe38499c1cb6baefa4dfa7d6
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:57 -0600

    Create Example_17.php

---
+++ PHPOperators/Example_17.php
@@ -0,0 +1,12 @@
+<!-- Comparison operator: Greater than (>) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 100;
+            $y = 50;
+
+            var_dump($x > $y); // returns true because $x is greater than $y
+        ?>  
+    </body>
+</html>
