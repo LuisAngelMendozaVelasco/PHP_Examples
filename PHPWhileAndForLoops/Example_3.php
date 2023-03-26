003f345e2b9769b1983733c711f84bd017bcafd7
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-26 10:36:28 -0600

    Create Example_3.php

---
+++ PHPWhileAndForLoops/Example_3.php
@@ -0,0 +1,14 @@
+<!-- Another do...while loop -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+            $x = 6;
+
+            do {
+                echo "The number is: $x <br>";
+                $x++;
+            } while ($x <= 5);
+        ?>
+    </body>
+</html>
