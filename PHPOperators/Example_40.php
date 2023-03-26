445872fb5c748da1ac3464aef5462fe9ab4a7374
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:15:01 -0600

    Create Example_40.php

---
+++ PHPOperators/Example_40.php
@@ -0,0 +1,15 @@
+<!-- Conditional assignment operator: Ternary (?:) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            // if empty($user) = TRUE, set $status = "anonymous"
+            echo $status = (empty($user)) ? "anonymous" : "logged in";
+            echo("<br>");
+
+            $user = "John Doe";
+            // if empty($user) = FALSE, set $status = "logged in"
+            echo $status = (empty($user)) ? "anonymous" : "logged in";
+        ?>  
+    </body>
+</html>
