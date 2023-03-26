5aae7bfc386c787ed3d390a6aa0954b3ce2d0959
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:14:05 -0600

    Create Example_20.php

---
+++ PHPOperators/Example_20.php
@@ -0,0 +1,12 @@
+<!-- Comparison operator: Less than or equal (<=) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 50;
+            $y = 50;
+
+            var_dump($x <= $y); // returns true because $x is less than or equal to $y
+        ?>  
+    </body>
+</html>
