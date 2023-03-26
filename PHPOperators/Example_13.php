9a515979820c10b0a0081a2c68a4ac3cf23e1b5c
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:40 -0600

    Create Example_13.php

---
+++ PHPOperators/Example_13.php
@@ -0,0 +1,12 @@
+<!-- Comparison operator: Identical (===) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 100;  
+            $y = "100";
+
+            var_dump($x === $y); // returns false because types are not equal
+        ?>  
+    </body>
+</html>
