345988ee7b8db8193ec9f3955fb5885e4f5dcd92
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:13 -0600

    Create Example_4.php

---
+++ PHPOperators/Example_4.php
@@ -0,0 +1,12 @@
+<!-- Arithmetic operator: Division (/) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 10;  
+            $y = 6;
+
+            echo $x / $y;
+        ?>  
+    </body>
+</html>
