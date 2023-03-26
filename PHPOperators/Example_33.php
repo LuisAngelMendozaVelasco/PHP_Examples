5d32d6e676227ca46f1758a3213a2f92bb27541d
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:14:44 -0600

    Create Example_33.php

---
+++ PHPOperators/Example_33.php
@@ -0,0 +1,12 @@
+<!-- String operator: Appends $txt2 to $txt1 -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $txt1 = "Hello";
+            $txt2 = " world!";
+            $txt1 .= $txt2;
+            echo $txt1;
+        ?>  
+    </body>
+</html>
