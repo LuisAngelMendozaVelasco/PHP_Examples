c868b9f5a0d10e9e7dc684af3c5d6c49d9342087
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-26 10:03:38 -0600

    Create Example_2.php

---
+++ PHPIf-ElseAndSwitchStatements/Example_2.php
@@ -0,0 +1,16 @@
+<!-- The if...else statement -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $t = date("H");
+
+            if ($t < "20") {
+                echo "Have a good day!";
+            } 
+            else {
+                echo "Have a good night!";
+            }
+        ?>
+    </body>
+</html>
