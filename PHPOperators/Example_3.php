552067bfd99d96f15ce5af4ea166e68ac1ea3844
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:07 -0600

    Create Example_3.php

---
+++ PHPOperators/Example_3.php
@@ -0,0 +1,12 @@
+<!-- Arithmetic operator: Multiplication (*) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 10;  
+            $y = 6;
+
+            echo $x * $y;
+        ?>  
+    </body>
+</html>
