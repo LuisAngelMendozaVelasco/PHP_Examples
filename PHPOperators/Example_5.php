4a76fee5b59e42b3655dbf7210b26cf193cb0ac9
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-25 20:13:17 -0600

    Create Example_5.php

---
+++ PHPOperators/Example_5.php
@@ -0,0 +1,12 @@
+<!-- Arithmetic operator: Modulus (%) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $x = 10;  
+            $y = 6;
+
+            echo $x % $y;
+        ?>  
+    </body>
+</html>
