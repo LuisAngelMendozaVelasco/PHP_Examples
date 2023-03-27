c2af0910ec7093abe2af9b1e6888f95f93fae635
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 15:30:42 -0600

    Create Example_2.php

---
+++ PHPSuperglobals/Example_2.php
@@ -0,0 +1,19 @@
+<!-- $_SERVER - Holds information about headers, paths, and script locations -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+            echo $_SERVER['PHP_SELF'];
+            echo "<br>";
+            echo $_SERVER['SERVER_NAME'];
+            echo "<br>";
+            echo $_SERVER['HTTP_HOST'];
+            echo "<br>";
+            echo $_SERVER['HTTP_REFERER'];
+            echo "<br>";
+            echo $_SERVER['HTTP_USER_AGENT'];
+            echo "<br>";
+            echo $_SERVER['SCRIPT_NAME'];
+        ?>
+    </body>
+</html>
