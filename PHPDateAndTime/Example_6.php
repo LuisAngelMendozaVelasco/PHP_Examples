dfc1b636da5640ca1c7dcbc5e19f76d95a3ec447
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 16:51:03 -0600

    Create Example_6.php

---
+++ PHPDateAndTime/Example_6.php
@@ -0,0 +1,10 @@
+<!-- Create a date and time from the strtotime() function -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $d = strtotime("10:30pm April 15 2014");
+            echo "Created date is " . date("Y-m-d h:i:sa", $d);
+        ?>
+    </body>
+</html>
