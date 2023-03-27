d8fd3fe24191865155aa6aa2a70d1844bd8ea8da
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 15:40:15 -0600

    Create Example_3.php

---
+++ PHPRegularExpressions/Example_3.php
@@ -0,0 +1,11 @@
+<!-- Replace "Microsoft" with "W3Schools" in a string -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $str = "Visit Microsoft!";
+            $pattern = "/microsoft/i";
+            echo preg_replace($pattern, "W3Schools", $str);
+        ?>
+    </body>
+</html>
