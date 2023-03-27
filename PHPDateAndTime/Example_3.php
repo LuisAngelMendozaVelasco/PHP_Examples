f1861ef51c7f809bd7cb1fe8ed4cb67e8151a7f1
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 16:50:56 -0600

    Create Example_3.php

---
+++ PHPDateAndTime/Example_3.php
@@ -0,0 +1,9 @@
+<!-- Output the current time (server time) -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "The time is " . date("h:i:sa");
+        ?>
+    </body>
+</html>
