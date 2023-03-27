e1b330a54776bf6ce2d4553ea669b65e0f5c279e
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 14:27:55 -0600

    Create Example_1.php

---
+++ PHPSortingArrays/Example_1.php
@@ -0,0 +1,16 @@
+<!-- sort() - Sort array in ascending alphabetical order -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $cars = array("Volvo", "BMW", "Toyota");
+            sort($cars);
+
+            $clength = count($cars);
+            for($x = 0; $x < $clength; $x++) {
+                echo $cars[$x];
+                echo "<br>";
+            }
+        ?>
+    </body>
+</html>
