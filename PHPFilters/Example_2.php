a3928829216e057f8922da5899efa3095525d1e5
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 21:35:40 -0600

    Create Example_2.php

---
+++ PHPFilters/Example_2.php
@@ -0,0 +1,11 @@
+<!-- Sanitize a string -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $str = "<h1>Hello World!</h1>";
+            $newstr = filter_var($str, FILTER_SANITIZE_STRING);
+            echo $newstr;
+        ?>
+    </body>
+</html>
