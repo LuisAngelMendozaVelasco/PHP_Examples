57c4fe2335d3e0fd142edb48e191c29d94f6ad28
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 18:58:48 -0600

    Create Example_2.php

---
+++ PHPIncludeFiles/Example_2.php
@@ -0,0 +1,13 @@
+<!-- Use include to include "menu.php" in a page -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="menu">
+            <?php include 'menu.php';?>
+        </div>
+
+        <h1>Welcome to my home page!</h1>
+        <p>Some text.</p>
+        <p>Some more text.</p>
+    </body>
+</html>
