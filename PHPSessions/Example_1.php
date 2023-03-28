924bcf736dd667f796cac012fd6a3d3bd52bac1e
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 21:00:34 -0600

    Create Example_1.php

---
+++ PHPSessions/Example_1.php
@@ -0,0 +1,16 @@
+<!-- Start a session -->
+<?php
+// Start the session
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            // Set session variables
+            $_SESSION["favcolor"] = "green";
+            $_SESSION["favanimal"] = "cat";
+            echo "Session variables are set.";
+        ?>
+    </body>
+</html>
