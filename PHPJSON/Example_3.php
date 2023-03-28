9f4be2653e1816ae1d4c3575604244832b6f4e41
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 22:22:39 -0600

    Create Example_3.php

---
+++ PHPJSON/Example_3.php
@@ -0,0 +1,15 @@
+<!-- Access the values from a PHP object -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $jsonobj = '{"Peter":35, "Ben":37, "Joe":43}';
+
+            $obj = json_decode($jsonobj);
+
+            echo $obj->Peter . ", ";
+            echo $obj->Ben . ", ";
+            echo $obj->Joe;
+        ?>
+    </body>
+</html>
