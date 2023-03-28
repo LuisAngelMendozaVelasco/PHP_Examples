ace9f544bf3b486bfd0bfab4f2239dd6f71f98fb
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-27 19:22:10 -0600

    Create Example_4.php

---
+++ PHPFileOpen-Read-Close/Example_4.php
@@ -0,0 +1,14 @@
+<!-- Use fgetc() to read a single character from a file -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+            // Output one character until end-of-file
+            while(!feof($myfile)) {
+                echo fgetc($myfile);
+            }
+            fclose($myfile);
+        ?>
+    </body>
+</html>
